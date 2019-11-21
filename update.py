05aa946134f3bd12405c33e5a2dd62ac0e94fc2b
lijian <173013905@qq.com>
2019-11-21 16:56:14 +0800

    first commit

---
+++ update.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import tkinter.messagebox
+import requests
+
+
+# VERSION_NOW='100'#1.0.0
+# UPDATE_DOWNLOAD_URL='http://www.url.com/a.zip' #新版本文件
+# VERSION_URL='http://www.url.com/version'#最新版本号
+
+# try:
+#     ver=request.get(VERSION_URL)#获取最新版本号
+#    #然后自己根据版本号对版本进行对比处理
+#     #这里省略
+#     #直接跳到更新代码
+#     tkinter.messagebox.showwarning(title='提示', message='发现新版本，点击确定开始更新。更新时间跟网速有关，请耐心等待！')
+#     newFile=requests.get(UPDATE_DOWNLOAD_URL)
+#         with open("newFile_update.zip","wb") as fp:
+#             fp.write(newFile.content)
+# except:
+#     tkinter.messagebox.showwarning(title='警告', message='更新失败，请检查网络！')
+#     tkinter.messagebox.showwarning(title='提示', message='新版本软件下载完成！请在当前软件目录查看(文件名：newFile_update.zip)并使用新版本。')
+
+
+
+# import os
+# main = "web.exe"
+# os.system(main)
